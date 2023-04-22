4da74e0474d711d74ac12fb7993490f0712aa07b
parag-radadiya <55325646+parag-radadiya@users.noreply.github.com>
2023-04-22 19:14:06 +0530

    this is our first commit for dao

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          Wel-come To Our DEO-web 
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
